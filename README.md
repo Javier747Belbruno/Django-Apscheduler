70228bd2410f3117f95d672f1de99437a18b7371
Javier <javierbelbruno@gmail.com>
2021-12-19 13:51:13 -0300

    changes

---
+++ README.md
@@ -0,0 +1,7 @@
+manage.py runapscheduler        
+
+
+start /b ping google.com & start /b ping example.com
+
+start /b python manage.py runserver & start /b python manage.py runapscheduler
+
---
+++ django_apscheduler/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+class DjangoApschedulerConfig(AppConfig):
+    name = "django_apscheduler"
+    verbose_name = "Django APScheduler"
+    #def ready(self):
+        #from django.core.management import call_command
+        #call runapscheduler command
+        #call_command('runapscheduler')
+
